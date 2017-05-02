bcd72f92644fe5e2aecde9093518b1c058a7af8e
Blue-Sky-r <robert.puskajler@yahoo.ca>
2017-05-02 20:10:44 +0200

    url check added

--- scripts/show-stream-title.lua
+++ scripts/show-stream-title.lua
@@ -1,24 +1,67 @@
--- Simple script for showing current stream Title OSD when moving through playlist
--- Note: show-text "${media-title}" displays playing title and not the current title
--- /see mpv doc for details between playing title and current title/
--- All OSD config options (duration, align, border, scale, ...) could be set in mpv.conf
+-- Show OSD stream title from playlist on change
 --
--- place in ~/.config/mpv/scripts/ for autoload
+-- Usefull for iPTV when switching the streams/channels defined in the playlist
+--
+-- At the moment of jump from stream1 to stream2 the property 'media-title' still contains stream1.title
+-- (see mpv doc for more details, lookup playlist/N/current, playlist/N/playing )
+-- To display correct stream2.title use this script.
+--
+-- Place into ~/.config/mpv/scripts/ for autoload
+--
+-- OSD options like duration, alignment, border, scale could be set in ~/.config/mpv/mpv.conf
+--
+-- SMPlayer playlist entry example:
+--   #EXTINF:0,RT News,,0
+--   http://rt-eng-live.hls.adaptive.level3.net/rt/eng/index1600.m3u8
+--
+-- GitHub: https://github.com/blue-sky-r/mpv/tree/master/scripts
 
--- convert stream names to upper-case
-local upcase = false
+-- OSD format string tokens:
+-- %N ... iPTV channel number (playlist index)
+-- %t ... iPTV channel name (user friendly stream title)
+-- %T ... iPTV channel name in uppercase
 
--- [observe] property 'media-title' changed to '"Title,,0"'
-mp.observe_property("media-title", "native", function(name, val)
+-- defaults
+local cfg = {
+	format = "%N. %t",
+    ignoreurl = true
+}
+
+-- quick check if string s is like url
+local function likeurl(s)
+    return string.match(s, "^%a%a%a+://%w+")
+end
+
+-- [show_stream_title] property 'media-title' changed to 'iptv-streams.m3u8'
+-- [show_stream_title] property 'media-title' changed to 'CP24,,0'
+--
+-- [show_stream_title] property 'media-title' changed to 'TA News,,0'
+-- [show_stream_title] property 'media-title' changed to 'rtmp://eo1-gts.ta.live.cc:1945/ta-o/_definst_/livem2'
+--
+-- [show_stream_title] property 'media-title' changed to 'EDU,:/default-theme/openfolder.png,1'
+-- [show_stream_title] property 'media-title' changed to 'History,,0'
+--
+mp.observe_property("media-title", "string",
+    function(name, val)
         -- log
-        mp.msg.log("info", "property '"..name.."' changed to '"..val.."'")
-        -- comma position
-        local compos = string.find(val, ",")
-        -- comma found
-        if compos then
-        	--  stream title from val / the first part: title,icon,0
-           	local txt = string.sub(val, 1, compos-1)
-           	if upcase then txt = string.upper(txt) end
-           	mp.osd_message(txt)
-	end
-end)
+        mp.msg.info("property '"..name.."' changed to '"..val.."'")
+        -- val can be url (redirects ?)
+        if cfg.ignoreurl and likeurl(val) then return end
+
+        -- SMPlayer playlist val = 'Title,,0'
+        --          playlist val = 'Title'
+
+        -- get comma position or entire length
+        local compos = string.find(val..",", ",")
+        -- stream title from val
+        local title = string.sub(val, 1, compos-1)
+        -- playlist index (0-based)
+        local n = 1 + mp.get_property('playlist-pos')
+        -- replace tokens
+        local txt = string.gsub(cfg.format, '%%N', n):gsub('%%t', title):gsub('%%T', string.upper(title))
+
+        -- osd show
+        mp.osd_message(txt)
+    end
+)
+
