75e29cb223f3d04073f5442797677cbb39ba5ad6
Blue-Sky-r <robert.puskajler@yahoo.ca>
2017-10-18 18:31:33 +0200

    working

--- scripts/channel-by-name.lua
+++ scripts/channel-by-name.lua
@@ -6,6 +6,12 @@
 -- To change this template use File | Settings | File Templates.
 --
 
+-- input.conf
+-- # key to channel name assignment
+-- c script-message-to channel_by_name channel CNN
+-- d script-message-to channel_by_name channel Discovery
+
+
 local options = require("mp.options")
 local utils   = require("mp.utils")
 
@@ -14,15 +20,12 @@ local cfg = {
     -- *s* ... case insensitive substring
     -- *s  ... case insensitive endswith
     -- s*  ... case insensitive startswith
-    -- s   ... case insebsitive string match
+    -- s   ... case insebsitive string equality
     -- *S* ... case sensitive substring
     -- *S  ... case sensitive endswith
     -- S*  ... case sensitive startswith
-    -- S   ... case sensitive string match
-	match    = "*s*",
-    -- helper function will bet
-    case_fn  = nil,
-    match_fn = nil
+    -- S   ... case sensitive string equality
+	match    = "*s*"
 }
 
 -- read lua-settings/channel-by-name.conf
@@ -42,40 +45,10 @@ end
 -- channel name to number lookup
 local chan_name2num = {}
 
--- playlist title to channel name
--- "title" = "RT News,,0" -> "channel-name" = "RT News"
-local function title2name(title)
-    -- add sentinel to title
-    local compos = string.find(title..",", ",")
-    -- slice substring from beginning up to the first comma
-    local name = string.sub(title, 1, compos-1)
-
-    return name
-end
-
--- create dictionary from playlist
-local function create_playlist_dict()
-
-    local playlist = mp.get_property_native("playlist")
-
-    mp.msg.verbose("playlist -> ".. utils.to_string(playlist))
-
-    for plnum,plval in ipairs(playlist) do
-        mp.msg.info("playlist key("..plnum..") -> val("..utils.to_string(plval)..")")
-        local chname = title2name(plval.title)
-        -- adjust case
-        if match_case_insensitive() then chname = chname:lower()
-        -- add to dictionary
-        table.insert(chan_name2num, {[chname] = plnum})
-    end
-
-    mp.msg.verbose("lookup -> ".. utils.to_string(chan_name2num))
-end
-
 local function lookup_name2num(name)
     -- lookup name to playlist-pos-1
     local pattern = match_pattern(name)
-    for chname,chnum in ipairs(chan_name2num) do
+    for chname,chnum in pairs(chan_name2num) do
         if chname:match(pattern) then
             return chnum
         end
@@ -85,17 +58,52 @@ end
 -- set channel by name
 local function channel(name)
     -- adjust case
-    if match_case_insensitive() name = name:lower()
+    if match_case_insensitive() then name = name:lower() end
     -- lookup playlist position
     local pos1 = lookup_name2num(name)
     --
-    mp.msg.verbose("channel:".. name .." -> ".. pos1)
+    mp.msg.info("channel:".. name .." -> ".. pos1)
     -- set playlist-pos-1
     if pos1 then
         mp.set_property("playlist-pos-1", pos1)
     end
 end
 
+-- playlist title to channel name
+-- "title" = "RT News,,0" -> "channel-name" = "RT News"
+local function title2name(title)
+    -- add sentinel to title
+    local compos = string.find(title..",", ",")
+    -- slice substring from beginning up to the first comma
+    local name = string.sub(title, 1, compos-1)
+    --
+    return name
+end
+
+-- create dictionary from playlist
+local function create_playlist_dict()
+
+    local playlist = mp.get_property_native("playlist")
+    local case_insensitive = match_case_insensitive()
+
+    --mp.msg.verbose("create_playlist_dict() playlist -> "..utils.to_string(playlist))
+
+    for plnum,plval in pairs(playlist) do
+        mp.msg.info("playlist channel num:"..plnum.." -> "..utils.to_string(plval))
+        -- channel name from playlist title
+        local chname = title2name(plval.title)
+        -- adjust case
+        if case_insensitive then chname = chname:lower() end
+        -- add to dictionary
+        chan_name2num[chname] = plnum
+    end
+
+    mp.msg.verbose("lookup chan_name2num -> ".. utils.to_string(chan_name2num))
+end
+
 -- after playlist has been loaded
 mp.register_event("file-loaded", create_playlist_dict)
--- create_playlist_dict()
+--
+mp.register_script_message("channel", channel)
+
+
