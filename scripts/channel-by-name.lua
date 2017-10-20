33e01d126b03428ae0b1bfa162044d8d18961ba1
Blue-Sky-r <robert.puskajler@yahoo.ca>
2017-10-20 08:54:46 +0200

    escape channel name

--- scripts/channel-by-name.lua
+++ scripts/channel-by-name.lua
@@ -25,7 +25,9 @@ local cfg = {
     -- *S  ... case sensitive endswith
     -- S*  ... case sensitive startswith
     -- S   ... case sensitive string equality
-	match    = "*s*"
+	match    = "*s*",
+    -- refresh channel lookup table on each channel change
+    refresh  = false
 }
 
 -- read lua-settings/channel-by-name.conf
@@ -38,8 +40,26 @@ local function match_case_insensitive()
     return cfg.match:find("s")
 end
 
+local case_insensitive = match_case_insensitive()
+
+-- escape special chars
+local function escape(str)
+  local esc = {
+    ["("] = "%%(",
+    [")"] = "%%)",
+    ["%"] = "%%%%",
+    ["."] = "%%.",
+    ["["] = "%%[",
+    ["]"] = "%%]",
+    ["*"] = "%%*",
+    ["+"] = "%%+",
+    ["-"] = "%%-"
+  }
+  return str:gsub(".", esc)
+end
+
 local function match_pattern(str)
-    return "^"..cfg.match:gsub("*", ".*"):gsub("[Ss]",str).."$"
+    return "^"..cfg.match:gsub("%*", ".*"):gsub("[Ss]", escape(str)).."$"
 end
 
 -- channel name to number lookup
@@ -58,14 +78,14 @@ end
 -- set channel by name
 local function channel(name)
     -- adjust case
-    if match_case_insensitive() then name = name:lower() end
+    if case_insensitive then name = name:lower() end
     -- lookup playlist position
-    local pos1 = lookup_name2num(name)
+    local num = lookup_name2num(name)
     --
-    mp.msg.info("channel:".. name .." -> ".. pos1)
     -- set playlist-pos-1
-    if pos1 then
-        mp.set_property("playlist-pos-1", pos1)
+    if num then
+        mp.msg.info("channel name:".. name .." -> num:".. num)
+        mp.set_property("playlist-pos-1", num)
     end
 end
 
@@ -81,29 +101,28 @@ local function title2name(title)
 end
 
 -- create dictionary from playlist
-local function create_playlist_dict()
+local function create_playlist_dict(event)
 
     local playlist = mp.get_property_native("playlist")
-    local case_insensitive = match_case_insensitive()
 
-    --mp.msg.verbose("create_playlist_dict() playlist -> "..utils.to_string(playlist))
-
-    for plnum,plval in pairs(playlist) do
-        mp.msg.info("playlist channel num:"..plnum.." -> "..utils.to_string(plval))
+    for num,val in pairs(playlist) do
+        mp.msg.verbose("playlist channel num:"..num.." -> "..utils.to_string(val))
+        if not val.title then return end
         -- channel name from playlist title
-        local chname = title2name(plval.title)
+        local chname = title2name(val.title)
         -- adjust case
         if case_insensitive then chname = chname:lower() end
         -- add to dictionary
-        chan_name2num[chname] = plnum
+        chan_name2num[chname] = num
     end
 
-    mp.msg.verbose("lookup chan_name2num -> ".. utils.to_string(chan_name2num))
+    mp.msg.info("lookup chan_name2num -> ".. utils.to_string(chan_name2num))
+
+    -- do not recreate lookup dictionary if refresh not required
+    if not cfg.refreh then mp.unregister_event(create_playlist_dict) end
 end
 
 -- after playlist has been loaded
 mp.register_event("file-loaded", create_playlist_dict)
 --
 mp.register_script_message("channel", channel)
-
-
