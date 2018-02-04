f1e757ac83ca5e570467ff4b48b066167911e4db
Blue-Sky-r <robert.puskajler@yahoo.ca>
2018-02-04 14:37:49 +0100

    added comments, reformatted code

--- scripts/channel-survey.lua
+++ scripts/channel-survey.lua
@@ -1,12 +1,20 @@
+-- Channel Survey - periodically switch channels in round-robbin fasion
 --
--- Created by IntelliJ IDEA.
--- User: robert
--- Date: 20.01.2018
--- Time: 8:57
--- To change this template use File | Settings | File Templates.
+-- Channel survey mode is periodic roundabout style channel switching.
+-- Usefull when you want to scan channels and are lazy pressing 'next-channel' button.
+-- Can be used for security iptv cameras around your home or as a quick check for ski resorts in your area.
+-- Configurable key (defaylt 'x') toggles active playlist to alternate survey playlist.
 --
-
--- https://github.com/mpv-player/mpv/blob/master/DOCS/man/lua.rst#events
+-- Channel survey configurable options:
+--   playlist ... alternate playlist used for channel survey
+--   duration ... how long [in seconds] to show each channel, fractional values supported
+--   key      ... to bind channel survey mode toggle (false for no binding; default 'x' key)
+--
+-- To customize configuration place channel-survey.conf template into ~/.config/mpv/lua-settings/ and edit
+--
+-- Place script into ~/.config/mpv/scripts/ for autoload
+--
+-- GitHub: https://github.com/blue-sky-r/mpv/tree/master/scripts
 
 local options = require("mp.options")
 local utils   = require("mp.utils")
@@ -25,25 +33,9 @@ options.read_options(cfg, 'channel-survey')
 mp.msg.verbose('cfg = '..utils.to_string(cfg))
 
 local saved = {
-    -- _properties_  = { 'playlist-pos-1', 'osd-duration' },
     playlist_pos  = nil,
     playlist_path = nil
 }
-
---
-local function save_properties()
-    for idx,name in pairs(saved._properties_) do
-        saved[name] = mp.get_property_native(name)
-        mp.msg.info('property '..name..'='..utils.to_string(mp.get_property_native(name)))
-    end
-end
-
---
-local function restore_properties()
-    for idx,name in pairs(saved._properties_) do
-        mp.set_property_native(name, saved[name])
-    end
-end
 
 -- round-robbin channel switching
 local function next_channel()
@@ -54,46 +46,64 @@ local function next_channel()
     mp.msg.verbose('next-channel() '..key..'='..pos)
 end
 
+-- periodic timer for channels switching
 local timer = mp.add_periodic_timer( cfg.duration, next_channel)
+
+-- just prepare timer and wait for keypress (trigger) so stop timer for now
 timer:kill()
 
+-- start channel survey
 local function survey_start()
+    -- save actual playlist position
     saved.playlist_pos = mp.get_property_native('playlist-pos')
-    --
+    -- load survey playlist
     mp.commandv("loadfile", cfg.playlist, "replace", "playlist-pos=0")
+    -- start timer
     timer:resume()
+    -- log
     mp.msg.info('survey_start() loadfile:' .. cfg.playlist)
 end
 
+-- end of channel survey
 local function survey_stop()
+    -- stop timer
     timer:kill()
-    --mp.set_property_native('playlist-pos', saved.playlist_pos)
+    -- load saved playlist and position
     mp.commandv("loadfile", saved.playlist_path, "replace", "playlist-pos="..saved.playlist_pos)
+    -- log
     mp.msg.info('survey_stop() loadfile:' .. saved.playlist_path .. ' playlist-pos=' .. saved.playlist_pos)
 end
 
+-- save active playlist to be restored after end of survey
 local function on_playlist_change(name, val)
     -- property 'playlist' changed to '{{"filename" = "../tv.m3u8", "playing" = true, "current" = true}}'
     local path = val[1]["filename"]
+    -- save valid path
     if path then
         saved.playlist_path = path
+        -- stop observing as we now already have playlist path
         mp.unobserve_property(on_playlist_change)
+        -- log
         mp.msg.verbose('on_playlist_change() playlist path=' .. path)
     end
 end
 
---
+-- channel survey toggle called on key press
 function channel_survey()
+    -- only activate if non-zero duration
     if cfg.duration then
     	-- log
 	    mp.msg.info('playlist name:'..cfg.playlist..', duration:'..cfg.duration..', timer.is_enambled:'..utils.to_string(timer:is_enabled()) )
-
+        -- toggle run->stop or stop->run
         if timer:is_enabled() then survey_stop() else survey_start() end
     end
 end
 
+-- catch active playlist
 mp.observe_property('playlist', 'native', on_playlist_change)
 
+-- key to activate channel survey mode
 if cfg.key then
+    -- bind survey to keypress
     mp.add_forced_key_binding(cfg.key, 'channel-survey', channel_survey)
 end
